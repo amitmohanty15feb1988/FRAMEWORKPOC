239411ae280c78dacac9f027af7510ba877a1fd7
Amit Mohanty <amit.mohanty@cognizant.com>
2020-08-07 16:23:34 +0530

    first commit

---
+++ FrameworkTemplate1/cts.framework/src/cts/framework/Replace.java
@@ -0,0 +1,10 @@
+package cts.framework;
+
+public class Replace {
+	public static String replace(String source, String oldchar,String newchar)
+	
+	{
+		return source.replace(oldchar, newchar);
+		
+	}
+}
---
+++ RESTFINALFRAMEWORK/cts.framework.orchestration/src/cts/framework/orchestration/Replace.java
@@ -0,0 +1,10 @@
+package cts.framework.orchestration;
+
+public class Replace {
+	public static String replace(String source, String oldchar,String newchar)
+	
+	{
+		return source.replace(oldchar, newchar);
+		
+	}
+}
